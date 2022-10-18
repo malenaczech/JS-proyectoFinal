83579ebdae65f3769e79c626b9b397c2e14327e2
malenaczech <malenaczech03@gmail.com>
2022-10-18 15:55:30 -0300

    nav bar

---
+++ data-base/fragance.js
@@ -0,0 +1,56 @@
+const fragance = [
+    {
+        id: 111, 
+        brand: Lancome, 
+        name:"IDOLE EDP - 50ML", 
+        price: 25000
+    },
+    {
+        id: 112, 
+        brand: "Gucci", 
+        name:"GUILTY EDP - 50ML", 
+        price: 27000
+    },
+    {
+        id: 113, 
+        brand: "Paco Rabbane", 
+        name: "OLYMPEA EDP - 80ML", 
+        price: 24000
+    },
+    {
+        id: 114,
+        brand: "Nina Ricci", 
+        name:"NINA EDP - 50ML", 
+        price: 15000
+    },
+    {
+        id: 115,
+        brand:"Nina Ricci", 
+        name: "LUNA EDP - 50ML", 
+        price: 15800
+    },
+    {
+        id:116, 
+        brand:"Nina Ricci", 
+        name:"NINA ROUGE EDP - 80ML", 
+        price: 18000
+    },
+    {
+        id: 117, 
+        brand:"Nina Ricci",
+        name: "MADMOISELLE RICCI EDP - 80ML", 
+        price: 17900
+    },
+    {
+        id:118, 
+        brand:"Givenchy", 
+        name:"L'INTERDIT EDP - 80ML", 
+        price: 22000
+    },
+    {
+        id: 119, 
+        brand:"Givenchy", 
+        name:"L'INTERDIT ROUGE EDP - 80ml", 
+        price: 24000
+    }
+]
